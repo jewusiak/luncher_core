e8b9f8e84bd1335f238bebc3d49ee525e7d3f172
Grzegorz Jewusiak <grzegorz@jewusiak.pl>
2024-05-06 20:54:26 +0200

    added uuid field to profile resonse (whoami)

--- src/main/java/pl/luncher/v3/luncher_core/common/model/responses/UserProfileResponse.java
+++ src/main/java/pl/luncher/v3/luncher_core/common/model/responses/UserProfileResponse.java
@@ -3,9 +3,12 @@ package pl.luncher.v3.luncher_core.common.model.responses;
 import lombok.Data;
 import pl.luncher.v3.luncher_core.common.domain.infra.AppRole;
 
+import java.util.UUID;
+
 
 @Data
 public class UserProfileResponse {
+    private UUID uuid;
     private String email;
     private AppRole role;
     private String firstName;
