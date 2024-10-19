babb5efac5e2d6a9804fdd5c30c7971a4ebb0d44
Jewusiak Grzegorz <grzegorz@jewusiak.pl>
2024-10-20 00:17:52 +0200

    added user to data seeder

--- src/main/java/pl/luncher/v3/luncher_core/configuration/DefaultDataGenerator.java
+++ src/main/java/pl/luncher/v3/luncher_core/configuration/DefaultDataGenerator.java
@@ -4,12 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import pl.luncher.v3.luncher_core.user.domainservices.UserPersistenceService;
+import pl.luncher.v3.luncher_core.user.model.AppRole;
+import pl.luncher.v3.luncher_core.user.model.User;
 
 @Slf4j
 @RequiredArgsConstructor
 @Component
 public class DefaultDataGenerator {
+
+  private final PasswordEncoder passwordEncoder;
+  private final UserPersistenceService userPersistenceService;
 
 //  private final UserRepository userRepository;
 //  private final PasswordEncoder passwordEncoder;
@@ -20,7 +27,18 @@ public class DefaultDataGenerator {
 
   @EventListener(ApplicationReadyEvent.class)
   public void seedData() {
-
+    var root = new User();
+    root.setRole(AppRole.SYS_ROOT);
+    root.setFirstName("Grzegorz");
+    root.setSurname("Root");
+    root.setEmail("root@luncher.pl");
+    root.setPasswordHash(passwordEncoder.encode("1234"));
+    root.setEnabled(true);
+    try {
+      userPersistenceService.save(root);
+    }catch (RuntimeException exception) {
+      log.info("Failed to save the user", exception);
+    }
     //USERS
 //    List<User> users = List.of(
 //        User.builder().firstName("Grzegorz").surname("Root").email("root@g.pl")
