3953ccb8527416deb9d74f432920c80b62e5a7b4
Jewusiak Grzegorz <grzegorz@jewusiak.pl>
2024-11-04 13:11:41 +0100

    added validations

--- src/main/java/pl/luncher/v3/luncher_core/controllers/dtos/user/requests/UserCreateRequest.java
+++ src/main/java/pl/luncher/v3/luncher_core/controllers/dtos/user/requests/UserCreateRequest.java
@@ -3,6 +3,7 @@ package pl.luncher.v3.luncher_core.controllers.dtos.user.requests;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import pl.luncher.v3.luncher_core.user.model.AppRole;
 
@@ -19,7 +20,8 @@ public class UserCreateRequest {
   @NotBlank
   private String surname;
 
-  @NotBlank //TODO: password security validation
+  @NotNull
+  @Size(min = 4)
   private String password;
 
   @NotNull
