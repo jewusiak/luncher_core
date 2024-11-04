965e2e26bd5e44100c288ed47824915ce25cc1b3
Jewusiak Grzegorz <grzegorz@jewusiak.pl>
2024-11-04 12:49:27 +0100

    added validations to PlaceTypeRequest

--- src/main/java/pl/luncher/v3/luncher_core/controllers/dtos/placetype/requests/PlaceTypeRequest.java
+++ src/main/java/pl/luncher/v3/luncher_core/controllers/dtos/placetype/requests/PlaceTypeRequest.java
@@ -1,5 +1,6 @@
 package pl.luncher.v3.luncher_core.controllers.dtos.placetype.requests;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class PlaceTypeRequest {
 
+  @NotBlank
   private String identifier;
+  @NotBlank
   private String iconName;
+  @NotBlank
   private String name;
 }
